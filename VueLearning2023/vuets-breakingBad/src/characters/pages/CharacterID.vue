c12f4a48177ac21aac7f224c79992272379b0de4
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2023-05-31 09:23:53 -0400

    ⚡created our projects folders/files structure | pushing PR..

---
+++ VueLearning2023/vuets-breakingBad/src/characters/pages/CharacterID.vue
@@ -0,0 +1,49 @@
+<!-- @CharacterID.vue -->
+<!-- ------------------------------------------ -->
+<script setup lang="ts">
+
+// ------------------------------------------------
+
+
+// setup _____________________________________
+
+
+// functions _________________________________
+
+</script>
+<!-- ------------------------------------------ -->
+
+<template>
+  <div class="CharacterIDContainer">
+    <span>-------------------------------------</span>
+    <h1>@CharacterID</h1>
+	  <span>-------------------------------------</span>
+  </div>
+</template>
+<!-- ------------------------------------------ -->
+
+<style lang="scss">
+.CharacterIDContainer {
+	background-color: #2f323a;
+	margin-top: 5rem;
+	margin-left: 210px;
+	padding: 0;
+	width: 100%;
+	border-radius: 15px;
+	// ______________________________________________
+	h1 {
+		padding: 15px 200px 15px 200px;
+		font-family: Liberation Mono for Powerline, sans-serif;
+		color: mediumpurple;
+		font-size: 2.2rem;
+		display: grid;
+		place-items: center;
+	}
+	
+	span {
+		color: mediumpurple;
+		font-size: 2.2rem;
+	}
+}
+</style>
+<!-- ------------------------------------------ -->
