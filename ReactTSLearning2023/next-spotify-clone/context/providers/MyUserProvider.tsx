29ed7ae500b663d4165c4216da41a02d4dac1bbe
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2023-06-18 22:48:38 -0400

    ⚡I kept getting 406 error. corrected by using .maybeSingle() in supabase | pushing PR..

--- ReactTSLearning2023/next-spotify-clone/context/providers/MyUserProvider.tsx
+++ ReactTSLearning2023/next-spotify-clone/context/providers/MyUserProvider.tsx
@@ -18,103 +18,87 @@ export type PropsType = {
 // _______________________________________________
 
 export const MyUserProvider = (props: PropsType) => {
-	const {
-		session,
-		isLoading: isLoadingUser,
-		supabaseClient: supabase,
-	} = useSessionContext();
-	
-	const user = useSupaUser();
-	const accessToken = session?.access_token ?? null;
-	const [isLoadingData, setIsLoadingData] = useState(false);
-	
-	const [userDetails, setUserDetails] = useState<UserDetailsType | null>(null);
-	const [subscription, setSubscription] = useState<SubscriptionType | null>(null);
-	
-	
-	const fetchUserDetails = () => (
-		supabase.from('users').select('*').single()
-	);
-	
-	const fetchSubscription = () => (
-		supabase
-			.from('subscriptions')
-			.select('*, prices(*, products(*))')
-			.in('status', ['trailing', 'active'])
-			.single()
-	);
-	
-	const fetchData = async () => {
-		// Check if user data is ready and not loading, and
-		// user details and subscription are not yet fetched
-		if (user && !isLoadingData && !userDetails && !subscription) {
-			// Indicate that data is being loaded
-			setIsLoadingData(true);
-			
+		const {
+			session,
+			isLoading: isLoadingUser,
+			supabaseClient: supabase,
+		} = useSessionContext();
+		
+		const user = useSupaUser();
+		const accessToken = session?.access_token ?? null;
+		const [isLoadingData, setIsLoadingData] = useState(false);
+		
+		const [userDetails, setUserDetails] = useState<UserDetailsType | null>(null);
+		const [subscription, setSubscription] = useState<SubscriptionType | null>(null);
+		
+		// _________________ [functions] __________________
+		const fetchUserDetails = () => (
+			supabase
+				.from('users')
+				.select('*')
+				.single()
+		);
+		
+		const fetchSubscription = () => (
+			supabase
+				.from('subscriptions')
+				.select('*, prices(*, products(*))')
+				.in('status', ['trialing', 'active'])
+				.maybeSingle());
+		
+		const fetchData = async () => {
 			try {
-				// Concurrently fetch user details and subscription using
-				// Promise.allSettled Promise.allSettled returns a promise
-				// that resolves after all the given promises have either
-				// resolved or rejected.
-				const [userDetailsResult, subscriptionResult] = await Promise.allSettled([
-					fetchUserDetails()
-					, fetchSubscription(),
-				]);
-				
-				// If the userDetailsResult promise was fulfilled (resolved successfully),
-				// then set the userDetails state with the fetched data, casting it to the
-				// appropriate type.
-				if (userDetailsResult.status === 'fulfilled') {
-					setUserDetails(userDetailsResult.value.data as UserDetailsType);
-				}
-				
-				// If the subscriptionResult promise was fulfilled (resolved successfully),
-				// then set the subscription state with the fetched data, casting it to the
-				// appropriate type.
-				if (subscriptionResult.status === 'fulfilled') {
-					setSubscription(subscriptionResult.value.data as SubscriptionType);
+				if (user && !isLoadingData && !userDetails && !subscription) {
+					setIsLoadingData(true);
+					
+					Promise.allSettled([fetchUserDetails(), fetchSubscription()])
+						.then((results) => {
+								const userDetailsPromise = results[ 0 ];
+								const subscriptionPromise = results[ 1 ];
+								
+								if (userDetailsPromise.status === 'fulfilled') {
+									setUserDetails(userDetailsPromise.value.data as UserDetailsType);
+								}
+								
+								if (subscriptionPromise.status === 'fulfilled') {
+									setSubscription(subscriptionPromise.value.data as SubscriptionType);
+								}
+								
+								setIsLoadingData(false);
+							},
+						);
+				} else if (!user && !isLoadingUser && !isLoadingData) {
+					setUserDetails(null);
+					setSubscription(null);
 				}
 			} catch ( error: unknown ) {
-				// If there's an error while fetching data
-				// or updating states, log the error message
+				// Ensure error is an instance of Error before accessing error.message
 				if (error instanceof Error) console.error(error.message);
+				// re-throw the error so it can be caught
+				// by the function calling authenticate
+				throw error;
 			} finally {
-				// Indicate that data loading has finished,
-				// regardless of whether it was successful or not
 				setIsLoadingData(false);
 			}
-		}
-		// _________________ [functions] ___________________
+		};
 		
-	};
-	
-	useEffect(() => {
-		// If the user is logged in, and we're not currently loading,
-		// and the userDetails and subscription haven't been fetched,
-		// then fetch the data
-		if (value.user && !isLoadingData && !userDetails && !subscription) {
-			// Execute the fetchData function
+		useEffect(() => {
 			fetchData().then();
-			// If the user is not logged in, and we're not currently loading
-			// the user or other data, clear the userDetails and subscription
-		} else if (!value.user && !value && !isLoadingData) {
-			setUserDetails(null);
-			setSubscription(null);
-		}
-	}, [user, isLoadingUser]);
-	
-	const value = {
-		accessToken,
-		user,
-		userDetails,
-		isLoading: isLoadingUser || isLoadingData,
-		subscription,
-	};
-	// _________________________________________________
-	return (
-		<UserContext.Provider value={ value } { ...props } />
-	);
-};
+		}, [user, isLoadingUser]);
+		
+		const value = {
+			accessToken,
+			user,
+			userDetails,
+			isLoading: isLoadingUser || isLoadingData,
+			subscription,
+		};
+		// _________________________________________________
+		return (
+			<UserContext.Provider value={ value } { ...props } />
+		);
+	}
+;
 // _______________________________________________
 
 export const useUser = () => {
