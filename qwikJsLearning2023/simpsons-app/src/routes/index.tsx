e2d56183b2acf0fd700bfd2e7a299d7e720eeacd
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2023-07-22 20:39:54 -0400

    ⚡working on a model for my current qwik app | pushing PR..

--- qwikJsLearning2023/simpsons-app/src/routes/index.tsx
+++ qwikJsLearning2023/simpsons-app/src/routes/index.tsx
@@ -29,14 +29,34 @@ export default component$(() => {
         looks so cool!)
       </div>
       {/*|====== enter-app button ======|*/ }
-      <Link class="btn-white hover-text-shadow" href="/home">
-        Enter App
+      <Link
+        class="btn-white hover-text-shadow active:opacity-10 transition-opacity duration-200"
+        href="/home"
+      >Enter App
       </Link>
       {/*|====== simpsons-images ======|*/ }
       <div class="flex gap-12">
-        <ImgBart class="hidden md:block object-contain" alt="Bart Simpson" />
-        <ImgHomer class="hidden md:block object-contain mt-12" alt="Homer Simpson" />
-        <ImgMoe class="object-contain mt-20 md:mt-0" alt="Moe" />
+        <img
+          class="hidden md:block object-contain"
+          src="/img/bart.png"
+          alt="Bart Simpson"
+          width="195"
+          height="291"
+        />
+        <img
+          class="hidden md:block object-contain mt-12"
+          src="/img/homer.png"
+          alt="Homer Simpson"
+          width="175"
+          height="287"
+        />
+        <img
+          class="object-contain mt-20 md:mt-0"
+          src="/img/moe.png"
+          alt="Moe"
+          width="224"
+          height="330"
+        />
       </div>
     </div>
   );
